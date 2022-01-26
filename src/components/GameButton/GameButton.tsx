a10e020ad00a72f73dec42f1d4128f73e2251aea
Gustavo França <gstvz.franca@gmail.com>
2022-01-26 11:10:00 -0300

    feat: created GameButton component

---
+++ src/components/GameButton/GameButton.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import * as S from './styles';
+
+export const GameButton = () => {
+  return <S.GameButton>Game</S.GameButton>;
+};
---
+++ src/components/GameButton/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const GameButton = styled.button`
+  background-color: #FFFFFF;
+  border: 2px solid;
+  border-radius: 100px;
+  font-size: 1.4rem;
+  font-style: inherit;
+  font-weight: inherit;
+  max-width: 11rem;
+  padding: 0.6rem;
+`
