fb457b2912af4205087c2ace97763c2b89a6cd1f
Gustavo França <gstvz.franca@gmail.com>
2022-01-26 14:03:02 -0300

    feat: created GameNumbers component

---
+++ src/components/GameNumbers/GameNumbers.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import * as S from './styles';
+
+export const GameNumbers = () => {
+  return (
+    <S.List>
+      <S.ListItem>
+        <S.NumberButton>01</S.NumberButton>
+      </S.ListItem>
+      <S.ListItem>
+        <S.NumberButton>02</S.NumberButton>
+      </S.ListItem>
+      <S.ListItem>
+        <S.NumberButton>03</S.NumberButton>
+      </S.ListItem>
+      <S.ListItem>
+        <S.NumberButton>04</S.NumberButton>
+      </S.ListItem>
+      <S.ListItem>
+        <S.NumberButton>05</S.NumberButton>
+      </S.ListItem>
+      <S.ListItem>
+        <S.NumberButton>06</S.NumberButton>
+      </S.ListItem>
+      <S.ListItem>
+        <S.NumberButton>07</S.NumberButton>
+      </S.ListItem>
+      <S.ListItem>
+        <S.NumberButton>08</S.NumberButton>
+      </S.ListItem>
+      <S.ListItem>
+        <S.NumberButton>09</S.NumberButton>
+      </S.ListItem>
+      <S.ListItem>
+        <S.NumberButton>10</S.NumberButton>
+      </S.ListItem>
+    </S.List>
+  );
+};
---
+++ src/components/GameNumbers/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const List = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+`
+
+export const ListItem = styled.li`
+  margin-top: 1rem;
+  margin-right: 1.5rem;
+`
+
+export const NumberButton = styled.button`
+  background-color: #ADC0C4;
+  border: none;
+  border-radius: 50%;
+  color: #FFFFFF;
+  font-size: 2rem;
+  height: 6rem;
+  width: 6rem;
+`
