b3ba2b1ec3b7288c8de9cab38a56a78585ac1177
Gustavo França <gstvz.franca@gmail.com>
2022-01-26 14:02:42 -0300

    feat: created GameDescription component

---
+++ src/components/GameDescription/GameDescription.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as S from './styles';
+
+export const GameDescription = () => {
+  return (
+    <S.Description>
+    <p>
+      <strong>Fill your bet</strong>
+    </p>
+    <p>
+      Mark as many numbers as you want up to a maximum of 50. Win by
+      hitting 15, 16, 17, 18, 19, 20 or none of the 20 numbers drawn.
+    </p>
+  </S.Description>  
+  );
+};
---
+++ src/components/GameDescription/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Description = styled.div`
+  font-size: 1.7rem;  
+  font-weight: normal;
+  max-width: 65rem;
+
+  & p {
+    font-size: 1.7rem;
+    margin-bottom: 1rem;
+  }
+`
