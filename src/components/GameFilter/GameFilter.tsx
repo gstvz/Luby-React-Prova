18e8def8b8488c1332cd8760a2cd910ba9098ab7
Gustavo França <gstvz.franca@gmail.com>
2022-01-27 15:11:55 -0300

    feat: made GameFilter dynamically create GameButtons, based on the games on the API

--- src/components/GameFilter/GameFilter.tsx
+++ src/components/GameFilter/GameFilter.tsx
@@ -1,6 +1,42 @@
-import React from 'react';
-import * as S from './styles';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getGamesData } from "../../store/gamesThunk";
+import * as S from "./styles";
+
+type GameData = {
+  id: number;
+  type: string;
+  description: string;
+  range: number;
+  price: number;
+  "max-number": number;
+  color: string;
+};
+
+type gamesState = {
+  games: {
+    min_cart_value: 0;
+    types: GameData[];
+  };
+};
 
 export const GameFilter = () => {
-  return <S.GameButton>Game</S.GameButton>;
+  const dispatch = useDispatch();
+  const games = useSelector((state: gamesState) => state.games.types);
+
+  useEffect(() => {
+    dispatch(getGamesData());
+  }, [dispatch]);
+
+  return (
+    <>
+      {games.map((game) => {
+        return (
+          <S.GameButton key={game.id} color={game.color}>
+            {game.type}
+          </S.GameButton>
+        );
+      })}
+    </>
+  );
 };
--- src/components/GameFilter/styles.ts
+++ src/components/GameFilter/styles.ts
@@ -4,10 +4,16 @@ export const GameButton = styled.button`
   background-color: #FFFFFF;
   border: 2px solid;
   border-radius: 100px;
+  color: ${props => props.color};
   font-size: 1.4rem;
   font-style: inherit;
   font-weight: inherit;
   margin-right: 2.5rem;
   max-width: 11rem;
-  padding: 0.6rem;
+  padding: 0.6rem 1rem;;
+
+  &:hover {
+    background-color: ${props => props.color};
+    color: #FFFFFF;
+  }
 `
