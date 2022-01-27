77cc2b744e08add1335738add27f9187517f2d5d
Gustavo França <gstvz.franca@gmail.com>
2022-01-27 15:13:38 -0300

    refactor: removed exceeding ListItem components from GameBet, to make it create NumberButtons dynamically

--- src/components/GameBet/GameBet.tsx
+++ src/components/GameBet/GameBet.tsx
@@ -17,33 +17,6 @@ export const GameBet = () => {
         <S.ListItem>
           <S.NumberButton>01</S.NumberButton>
         </S.ListItem>
-        <S.ListItem>
-          <S.NumberButton>02</S.NumberButton>
-        </S.ListItem>
-        <S.ListItem>
-          <S.NumberButton>03</S.NumberButton>
-        </S.ListItem>
-        <S.ListItem>
-          <S.NumberButton>04</S.NumberButton>
-        </S.ListItem>
-        <S.ListItem>
-          <S.NumberButton>05</S.NumberButton>
-        </S.ListItem>
-        <S.ListItem>
-          <S.NumberButton>06</S.NumberButton>
-        </S.ListItem>
-        <S.ListItem>
-          <S.NumberButton>07</S.NumberButton>
-        </S.ListItem>
-        <S.ListItem>
-          <S.NumberButton>08</S.NumberButton>
-        </S.ListItem>
-        <S.ListItem>
-          <S.NumberButton>09</S.NumberButton>
-        </S.ListItem>
-        <S.ListItem>
-          <S.NumberButton>10</S.NumberButton>
-        </S.ListItem>
       </S.List>
     </S.Container>
   );
