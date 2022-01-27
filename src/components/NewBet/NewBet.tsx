c01a048415eca2ead80dfbab734fb1353218a0e5
Gustavo França <gstvz.franca@gmail.com>
2022-01-27 15:10:28 -0300

    feat: implemented useEffect to dispatch GET request and set games list

--- src/components/NewBet/NewBet.tsx
+++ src/components/NewBet/NewBet.tsx
@@ -1,34 +1,42 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import { getGamesData } from "../../store/gamesThunk";
 import { GameActions } from "../GameActions/GameActions";
 import { GameChoice } from "../GameChoice/GameChoice";
 import { GameBet } from "../GameBet/GameBet";
 import * as S from "./styles";
 
+type GameData = {
+  id: number;
+  type: string;
+  description: string;
+  range: number;
+  price: number;
+  "max-number": number;
+  color: string;
+};
+
 type gamesState = {
   games: {
-    min_cart_value: 0,
-    types: []
-  }
-}
+    min_cart_value: 0;
+    types: GameData[];
+  };
+};
 
 export const NewBet = () => {
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
   const games = useSelector((state: gamesState) => state.games.types);
-  
-  useEffect(() => {    
-    dispatch(getGamesData());    
-  }, [dispatch]);
 
-  console.log(games);
+  useEffect(() => {
+    dispatch(getGamesData());
+  }, [dispatch]);
 
   return (
     <S.NewBet>
       <S.Title>
         <strong>NEW BET</strong> FOR MEGASENA
       </S.Title>
-      <GameChoice />
+      <GameChoice games={games} />
       <GameBet />
       <GameActions />
     </S.NewBet>
--- src/store/games.ts
+++ src/store/games.ts
@@ -1,23 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 type GameData = {
+  id: number;
   type: string;
   description: string;
   range: number;
   price: number;
   "max-number": number;
   color: string;
-}
+};
 
 type GamesState = {
-  min_cart_value: number,
+  min_cart_value: number;
   types: GameData[];
 };
 
 const initialState: GamesState = {
   min_cart_value: 0,
-  types: []
-}
+  types: [],
+};
 
 const gamesSlice = createSlice({
   name: "games",
@@ -26,8 +27,8 @@ const gamesSlice = createSlice({
     getGames(state, action) {
       state.min_cart_value = action.payload.min_cart_value;
       state.types = action.payload.types;
-    }
-  }
+    },
+  },
 });
 
 export const gamesActions = gamesSlice.actions;
