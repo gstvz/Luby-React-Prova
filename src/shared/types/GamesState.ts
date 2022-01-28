73cb35a04adee4dca7f4f8024b4cb3d4da401bbb
Gustavo França <gstvz.franca@gmail.com>
2022-01-28 14:06:14 -0300

    refactor: imported GameData type to GamesState activeGame prop

--- src/shared/types/GamesState.ts
+++ src/shared/types/GamesState.ts
@@ -4,14 +4,7 @@ export type GamesState = {
   games: {
     min_cart_value: 0;
     types: GameData[];
-    activeGame: {
-      id: 0;
-      type: "";
-      description: "";
-      range: 0;
-      price: 0;
-      "max-number": 0;
-      color: "";
-    };
+    activeGame: GameData;
+    selectedNumbers: string[];
   };
 };
