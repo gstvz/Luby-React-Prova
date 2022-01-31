56989e4597cc3cf1ddd20db6784ec9105b23c0fe
Gustavo França <gstvz.franca@gmail.com>
2022-01-31 16:29:05 -0300

    feat: changed UserData to LoginData and removed isAuthenticated; created UserData and UserState

--- src/shared/types/UserData.ts
+++ src/shared/types/UserData.ts
@@ -1,5 +1,17 @@
-export type UserData = {
+export type LoginData = {
   email?: string,
-  password?: string,
-  isAuthenticated: boolean
+  password?: string
+};
+
+export type UserData = {
+  isAuthenticated: boolean;
+  user: {
+    id: number,
+    email: string,
+    token: string;
+  }
+};
+
+export type UserState = {
+  user: UserData;
 };
