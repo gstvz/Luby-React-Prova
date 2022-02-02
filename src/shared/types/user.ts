d9864851e4502113c9c3efc51530e35534dde5d8
Gustavo França <gstvz.franca@gmail.com>
2022-02-02 14:48:11 -0300

    feat: created UserBets type, to type userBets state prop

--- src/shared/types/user.ts
+++ src/shared/types/user.ts
@@ -3,8 +3,19 @@ export type LoginData = {
   password?: string
 };
 
+export type UserBets = {
+  choosen_numbers: string;
+  created_at: string;
+  game_id: number;
+  id: number;
+  price: number;
+  type: {id: number, type: string};
+  user_id: number;
+}[]
+
 export type UserData = {
   isAuthenticated: boolean;
+  userBets: UserBets;
   user: {
     id: number | null,
     email: string | null,
