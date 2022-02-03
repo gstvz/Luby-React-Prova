b2fc8a452187395fd23dcfac5076c4f4e265f067
Gustavo França <gstvz.franca@gmail.com>
2022-02-03 10:58:56 -0300

    style: changed display flex to grid

--- src/pages/BetPage/styles.ts
+++ src/pages/BetPage/styles.ts
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
-  display: flex;
-  gap: 4rem;
-  justify-content: center;
+  display: grid;
+  column-gap: 4rem;
+  grid-template-columns: 1fr 34rem;
+  min-height: calc(100vh - 14rem);
   margin: 0 auto;
-  max-width: 135rem;
-  min-height: calc(100vh - 15rem);
-  width: 100%;
+  max-width: 120rem;
+  padding: 2rem 0;
+  width: 75%;
 `
