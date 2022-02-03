4751cd17f59bdf449bcbe8c645eee5a02573719e
Gustavo França <gstvz.franca@gmail.com>
2022-02-03 11:22:00 -0300

    style: removed margin-top and changed Title margin-bottom to 1rem

--- src/components/NewBet/styles.ts
+++ src/components/NewBet/styles.ts
@@ -4,13 +4,12 @@ export const NewBet = styled.section`
   display: flex;
   flex-direction: column;
   height: 100%;
-  margin-top: 4rem;
   max-width: 120rem;  
 `
 
 export const Title = styled.h2`
   font-size: 2.4rem;
   font-weight: normal;
-  margin-bottom: 2rem;
+  margin-bottom: 1rem;
   margin-right: 4.5rem;
 `
