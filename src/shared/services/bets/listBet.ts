d053fc18cf5f78c6ba9b744c0863c45d861ffd18
Gustavo França <gstvz.franca@gmail.com>
2022-02-07 13:41:06 -0300

    refactor: wrapped listBet statements on a try/catch block

--- src/shared/services/bets/listBet.ts
+++ src/shared/services/bets/listBet.ts
@@ -1,11 +1,15 @@
 import { api } from "@services";
-import { getUserToken } from "@helpers";
+import { errorMessage, getUserToken } from "@helpers";
 
 export const listBet = async (params: string) => {
-  const response = await api.get(`bet/all-bets${params}`, {
-    headers: {
-      Authorization: `Bearer ${getUserToken()}`,
-    },
-  });
-  return response.data;
+  try {
+    const response = await api.get(`bet/all-bets${params}`, {
+      headers: {
+        Authorization: `Bearer ${getUserToken()}`,
+      },
+    });
+    return response.data;
+  } catch(error) {
+    errorMessage(error);
+  }
 };
