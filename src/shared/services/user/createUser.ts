6e8614ed3f7f18e43b180daf77fe9d2ad7842b98
Gustavo França <gstvz.franca@gmail.com>
2022-02-07 10:58:22 -0300

    feat: implemented promise toast on createUser service

--- src/shared/services/user/createUser.ts
+++ src/shared/services/user/createUser.ts
@@ -1,7 +1,15 @@
+import { toast } from 'react-toastify';
 import { api } from "@services";
 import { NewUser } from "@types";
 
-export const createUser = async (newUser: NewUser) => {
-  const response = await api.post("user/create", newUser);
+export const createUser = async (newUser: NewUser) => {  
+  const response = await toast.promise(
+    api.post("user/create", newUser),
+    {
+      pending: 'Registering user...',
+      success: 'User registered 👌',
+      error: 'Registration failed 🤯'
+    }
+  )
   return response;
 };
