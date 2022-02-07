cd68f075c93a957a0820b090514d28b8b0352ea0
Gustavo França <gstvz.franca@gmail.com>
2022-02-07 13:25:52 -0300

    refactor: wrapped loginUser statements on a try/catch block

--- src/shared/services/auth/loginUser.ts
+++ src/shared/services/auth/loginUser.ts
@@ -1,13 +1,17 @@
 import { api } from "../axiosConfig";
 import { LoginData } from "@types";
+import { errorMessage } from "@helpers";
 
 export const loginUser = async (loginData: LoginData) => {
-    const response = await api.post("login", loginData);
-
+  try {
+    const response = await api.post("login", loginData);    
     return {
       id: response.data.user.id,
       email: response.data.user.email,
       token: response.data.token.token,
       expires_at: response.data.token.expires_at,
     };
+  } catch(error) {
+    errorMessage(error);
+  }
 };
