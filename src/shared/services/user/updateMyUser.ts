8e4fbde7b2e0fd97250c041e63eb530adc28baee
Gustavo França <gstvz.franca@gmail.com>
2022-02-07 10:47:25 -0300

    feat: implemented promise toast on updateMyUser service

--- src/shared/services/user/updateMyUser.ts
+++ src/shared/services/user/updateMyUser.ts
@@ -1,12 +1,21 @@
+import { toast } from 'react-toastify';
 import { api } from "@services";
 import { UpdatedUser } from "@types";
 import { getUserToken } from "@helpers";
 
 export const updateMyUser = async (updatedUser: UpdatedUser) => {
-  const response = await api.put("user/update", updatedUser, {
-    headers: {
-      Authorization: `Bearer ${getUserToken()}`,
+  const response = await toast.promise(
+    api.put("user/update", updatedUser, {
+      headers: {
+        Authorization: `Bearer ${getUserToken()}`,
+      }
+    }),
+    {
+      pending: 'Updating user...',
+      success: 'User updated 👌',
+      error: 'Update failed 🤯'
     }
-  });
+)
+  
   return response;
 };
