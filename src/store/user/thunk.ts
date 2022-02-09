dca0296e74f11aeef8ca97881ebfff8f3b0aaab4
Gustavo França <gstvz.franca@gmail.com>
2022-02-09 11:22:23 -0300

    refactor: deleted postResetPassword thunk because the service substituted it

--- src/store/user/thunk.ts
+++ src/store/user/thunk.ts
@@ -3,7 +3,6 @@ import { userActions } from "@store";
 import {
   listBet,
   loginUser,
-  resetPassword,
 } from "@services";
 
 export const postUserData = (loginData: LoginData) => {
@@ -11,16 +10,6 @@ export const postUserData = (loginData: LoginData) => {
     const userData = await loginUser(loginData);
     if(userData) {
       dispatch(userActions.loginUser(userData));
-    }
-  };
-};
-
-export const postResetPassword = (userEmail: { email: string }) => {
-  return async (dispatch: Function) => {
-    const response = await resetPassword(userEmail);
-
-    if (response.status === 200) {
-      alert("Link para resetar a senha enviado!");
     }
   };
 };
