b970d1c7faf86ac248468b1f32f8beddf71eb9cf
Gustavo França <gstvz.franca@gmail.com>
2022-02-09 14:05:28 -0300

    refactor: implemented promise handling with then function on createUser to set the feedback message toast

--- src/shared/services/user/createUser.ts
+++ src/shared/services/user/createUser.ts
@@ -1,18 +1,15 @@
 import { toast } from 'react-toastify';
 import { api } from "@services";
 import { NewUser } from "@types";
-import { errorMessage } from '@helpers';
+import { errorMessage, feedbackMesssage } from '@helpers';
 
 export const createUser = async (newUser: NewUser) => {  
   try {
-    const response = await toast.promise(
-      api.post("user/create", newUser),
-      {
-        pending: 'Registering user...',
-        success: 'User registered 👌'
-      }
-    )
-    return response;
+    const feedback = toast.loading("Registering user...");
+    await api.post("user/create", newUser)
+    .then(res => {
+      feedbackMesssage(feedback, res, "User registered! 👌");
+    });
   } catch(error) {
     errorMessage(error);
   }
