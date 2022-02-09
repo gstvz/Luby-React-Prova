4b6dc15f3cb58430c9c67729034db675d4775eb3
Gustavo França <gstvz.franca@gmail.com>
2022-02-09 09:52:43 -0300

    fix: declared registerSchema inside RegisterForm so it can access isAccount prop to conditionally add password validation based on what page it is (register or account); deleted registerSchema file

--- src/components/Forms/RegisterForm.tsx
+++ src/components/Forms/RegisterForm.tsx
@@ -1,29 +1,50 @@
 import React from "react";
+import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
-import { registerSchema } from "@schemas";
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as S from "./styles";
 import { createUser, updateMyUser } from "@services";
 
 type RegisterFormProps = {
   isAccount?: boolean;
-}
+};
 
 type RegisterInputs = {
-  name?: string,
-  email: string,
-  password?: string
-}
+  name: string;
+  email: string;
+  password?: string;
+};
 
 export const RegisterForm = ({ isAccount }: RegisterFormProps) => {
-  const navigate = useNavigate();
-  const { register, handleSubmit, formState: { errors }} = useForm<RegisterInputs>({
-    resolver: yupResolver(registerSchema)
+  const registerSchema = yup.object({
+    name: yup
+      .string()
+      .max(50, "Must have a maximum of 50 characters.")
+      .matches(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ'\s]+$/, {
+        message: "Must contain only letters, acentuation and spaces.",
+      })
+      .required("Required field."),
+    email: yup.string().email("Invalid email.").required("Required field."),
+    ...(!isAccount && {
+      password: yup
+        .string()
+        .max(6, "Must have a maximum of 6 characters.")
+        .required("Required field."),
+    }),
   });
 
-  const onSubmit: SubmitHandler<RegisterInputs> = async(userData) => {
-    if(isAccount) {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterInputs>({
+    resolver: yupResolver(registerSchema),
+  });
+
+  const onSubmit: SubmitHandler<RegisterInputs> = async (userData) => {
+    if (isAccount) {
       await updateMyUser(userData);
       navigate("/");
     }
@@ -53,8 +74,8 @@ export const RegisterForm = ({ isAccount }: RegisterFormProps) => {
             {...register("email")}
           />
           <S.InvalidInput>{errors.email?.message}</S.InvalidInput>
-        </S.Label>        
-        {!isAccount ?
+        </S.Label>
+        {!isAccount ? (
           <S.Label htmlFor="password">
             <S.Input
               type="password"
@@ -63,9 +84,8 @@ export const RegisterForm = ({ isAccount }: RegisterFormProps) => {
               {...register("password")}
             />
             <S.InvalidInput>{errors.password?.message}</S.InvalidInput>
-          </S.Label> :
-          ''
-        }
+          </S.Label>
+        ) : null}
         <S.Button>
           {isAccount ? "Save" : "Register"}
           <S.ArrowRight />
--- src/shared/schemas/index.ts
+++ src/shared/schemas/index.ts
@@ -1,4 +1,3 @@
 export * from "./loginSchema";
-export * from "./registerSchema";
 export * from "./resetSchema";
 export * from "./changeSchema";
