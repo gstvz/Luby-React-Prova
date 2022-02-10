11b45c6aab28ad2928009f15f7edaeba651b153f
Gustavo França <gstvz.franca@gmail.com>
2022-02-10 16:00:01 -0300

    refactor: changed password's max validation message

--- src/shared/schemas/loginSchema.ts
+++ src/shared/schemas/loginSchema.ts
@@ -4,6 +4,6 @@ export const loginSchema = yup.object({
   email: yup.string().email("Invalid email.").required("Required field."),
   password: yup
     .string()
-    .max(6, "Invalid password.")
+    .max(6, "Must have a maximum of 6 characters.")
     .required("Required field."),
 });
